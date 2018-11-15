2d7042cf7eb4e7943edc9b9fa3bb4974482bc617
marysia <mrbaranowska@gmail.com>
2018-11-15 11:10:37 +0100

    add book form finally works

--- bukifaj/bukifaj_app/static/bukifaj_app/book_modal.js
+++ bukifaj/bukifaj_app/static/bukifaj_app/book_modal.js
@@ -1,19 +1,54 @@
 $(function () {
 
-  $(".js-create-book").click(function () {
-    var form = $(this);
-    console.log(form)
+  /* Functions */
+
+  var loadForm = function () {
+  console.log('in')
+    var btn = $(this);
+    console.log(btn)
     $.ajax({
-      url: 'add_book',
+      url: btn.attr("data-url"),
       type: 'get',
       dataType: 'json',
       beforeSend: function () {
+        console.log('before send')
+        $("#modal-book .modal-content").html("");
         $("#modal-book").modal("show");
       },
       success: function (data) {
+        console.log('success')
         $("#modal-book .modal-content").html(data.html_form);
       }
     });
-  });
+  };
 
+  var saveForm = function () {
+    var form = $(this);
+    $.ajax({
+      url: form.attr("action"),
+      data: form.serialize(),
+      type: form.attr("method"),
+      dataType: 'json',
+      success: function (data) {
+        if (data.form_is_valid) {
+          $("#book-table tbody").html(data.html_book_list);
+          $("#modal-book").modal("hide");
+          $('#modal-book').on('hidden.bs.modal', function () {
+                location.reload();
+            });
+        }
+        else {
+          $("#modal-book .modal-content").html(data.html_form);
+        }
+      }
+    });
+    return false;
+  };
+
+
+  /* Binding */
+
+  // Create book
+  $(".js-create-book").click(loadForm);
+  $("#modal-book").on("submit", ".js-book-create-form", saveForm);
 });
